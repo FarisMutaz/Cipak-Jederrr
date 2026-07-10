39a8e94026d6bbbca52fc33c5ca7089ba7bfc333
FryzzMtz <farismutaz92@gmail.com>
2026-07-10 15:17:44 +0700

    fix: logo tidak muncul - exclude static files dari auth middleware

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,16 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Logo({ className = "w-32 h-32" }: { className?: string }) {
   return (
     <div className={`relative transition-transform hover:scale-105 duration-300 ${className}`}>
-      <img
+      <Image
         src="/logo.png"
         alt="Cipak Jederrr Logo"
+        width={128}
+        height={128}
         className="w-full h-full object-contain"
+        priority
       />
     </div>
   );
